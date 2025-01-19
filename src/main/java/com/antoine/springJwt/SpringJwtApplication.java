88f0b90baa58f35861d1462972597b7c0a12c734
AntoineMHB <antoinemuhumbania@gmail.com>
2025-01-19 23:32:14 +0200

    Errors in springJwt and app.prop corrected

--- src/main/java/com/antoine/springJwt/SpringJwtApplication.java
+++ src/main/java/com/antoine/springJwt/SpringJwtApplication.java
@@ -2,10 +2,12 @@ package com.antoine.springJwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.antoine.springJwt.repository")
+@EntityScan("com.antoine.springJwt.entity")
 public class SpringJwtApplication {
 
 	public static void main(String[] args) {
